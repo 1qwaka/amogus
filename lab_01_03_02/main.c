be13fd105c6b292a27306a938c8fa5d252b1302e
sas21u901 <sas21u901@student.bmstu.ru>
2022-02-28 12:45:52 +0300

    task 3 added

---
+++ lab_01_03_02/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    double r1, r2, r3, r;
+    scanf("%lf%lf%lf", &r1, &r2, &r3);
+    r = 1/(1/r1 + 1/r2 + 1/r3);
+    printf("%.6lf", r);
+
+    return 0;
+}
