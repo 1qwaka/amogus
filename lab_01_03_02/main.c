cc8bd209e78819de0356f89d52ebd2b2bcdfc19e
sas21u901 <sas21u901@student.bmstu.ru>
2022-02-28 13:05:55 +0300

    task 3 style fix

--- lab_01_03_02/main.c
+++ lab_01_03_02/main.c
@@ -5,7 +5,7 @@ int main(void)
 {
     double r1, r2, r3, r;
     scanf("%lf%lf%lf", &r1, &r2, &r3);
-    r = 1/(1/r1 + 1/r2 + 1/r3);
+    r = 1 / (1 / r1 + 1 / r2 + 1 / r3);
     printf("%.6lf", r);
 
     return 0;
