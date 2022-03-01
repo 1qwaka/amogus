be6536238caba5bf597aae5cad97a1dce0c75a57
sas21u901 <sas21u901@student.bmstu.ru>
2022-03-01 22:03:48 +0300

    task 2: perimeter find logic moved into separate function

--- lab_01_02_02/main.c
+++ lab_01_02_02/main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 
+double triangle_perimeter(double ax, double ay, double bx, double by, double cx, double cy);
+
 int main(void)
 {
     double ax, ay, bx, by, cx, cy;
     double p;
     scanf("%lf%lf%lf%lf%lf%lf", &ax, &ay, &bx, &by, &cx, &cy);
 
-    p = sqrt((ax - bx) * (ax - bx) + (ay - by) * (ay - by)) +
-        sqrt((ax - cx) * (ax - cx) + (ay - cy) * (ay - cy)) +
-        sqrt((cx - bx) * (cx - bx) + (cy - by) * (cy - by));
-    printf("%.6lf", p);
+    p = triangle_perimeter(ax, ay, bx, by, cx, cy);
 
+    printf("%.6lf", p);
     return 0;
+}
+
+double triangle_perimeter(double ax, double ay, double bx, double by, double cx, double cy)
+{
+    return  sqrt((ax - bx) * (ax - bx) + (ay - by) * (ay - by)) +
+            sqrt((ax - cx) * (ax - cx) + (ay - cy) * (ay - cy)) +
+            sqrt((cx - bx) * (cx - bx) + (cy - by) * (cy - by));
 }
