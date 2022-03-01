e90e18f545939efbf84a774be5242418341536b5
sas21u901 <sas21u901@student.bmstu.ru>
2022-03-01 22:11:13 +0300

    task 4: hours, minutes, seconds find logic moved into separate function

--- lab_01_04_02/main.c
+++ lab_01_04_02/main.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <math.h>
 
+int get_hours(double seconds);
+
+int get_minutes(double seconds);
+
+int get_seconds(double seconds);
+
 int main(void)
 {
     double seconds = 0.0;
     int hours = 0, minutes = 0, int_secs = 0;
+
     scanf("%lf", &seconds);
-    hours = seconds / 3600;
-    minutes = ((int)seconds % 3600) / 60;
-    int_secs = (int)seconds % 60;
+
+    hours = get_hours(seconds);
+    minutes = get_minutes(seconds);
+    int_secs = get_seconds(seconds);
 
     printf("%d %d %d", hours, minutes, int_secs);
 
     return 0;
+}
+
+
+int get_hours(double seconds)
+{
+    return seconds / 3600;
+}
+
+int get_minutes(double seconds)
+{
+    return ((int)seconds % 3600) / 60
+}
+
+int get_seconds(double seconds)
+{
+    return (int)seconds % 60;
 }
