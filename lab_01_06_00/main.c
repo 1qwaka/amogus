c22262fddf22f2bc4db032dc78aade74e2f374f1
sas21u901 <sas21u901@student.bmstu.ru>
2022-03-01 22:42:03 +0300

    task 6: triangle type definition logic moved into separate function, camel case switched to snake case

--- lab_01_06_00/main.c
+++ lab_01_06_00/main.c
@@ -3,54 +3,61 @@
 
 #define ERR_IN 1
 #define FLOAT_ERR 1e-6
-#define or ||
+
+short triangle_type(double ax, double ay, double bx, double by, double cx, double cy);
 
 int main(void)
 {
     double ax = 0, ay = 0, bx = 0, by = 0, cx = 0, cy = 0;
     short rc = 0;
-    int exitCode = 0;
+    int exit_code = 0;
 
     rc = scanf("%lf%lf%lf%lf%lf%lf", &ax, &ay, &bx, &by, &cx, &cy);
     if (rc != 6)
     {
-        exitCode = ERR_IN;
+        exit_code = ERR_IN;
     }
     else
     {
-        double tmp = 0;
-        double sideA = 0, sideB = 0, sideC = 0;
-        short result = 0;
-
-        sideA = sqrt((bx - cx) * (bx - cx) + (by - cy) * (by - cy));
-        sideB = sqrt((ax - cx) * (ax - cx) + (ay - cy) * (ay - cy));
-        sideC = sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay));
-
-        if (sideC > sideB)
-        {
-            tmp = sideB;
-            sideB = sideC;
-            sideC = tmp;
-        }
-        if (sideB > sideA)
-        {
-            tmp = sideA;
-            sideA = sideB;
-            sideB = tmp;
-        }
-
-        tmp = sideB * sideB + sideC * sideC;
-        result = 2;
-        if (fabs(tmp - sideA * sideA) < FLOAT_ERR)
-            result = 1;
-        else if (tmp > sideA * sideA)
-            result = 0;
-
-
+        short result = triangle_type(ax, ay, bx, by, cx, cy);
         printf("%d", result);
     }
 
+    return exit_code;
+}
 
 
-    return exitCode;
+short triangle_type(double ax, double ay, double bx, double by, double cx, double cy)
+{
+    double tmp = 0;
+    double side_a = 0, side_b = 0, side_c = 0;
+    short result = 0;
+
+    side_a = sqrt((bx - cx) * (bx - cx) + (by - cy) * (by - cy));
+    side_b = sqrt((ax - cx) * (ax - cx) + (ay - cy) * (ay - cy));
+    side_c = sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay));
+
+    if (side_c > side_b)
+    {
+        tmp = side_b;
+        side_b = side_c;
+        side_c = tmp;
+    }
+    if (side_b > side_a)
+    {
+        tmp = side_a;
+        side_a = side_b;
+        side_b = tmp;
+    }
+
+    tmp = side_b * side_b + side_c * side_c;
+    // тупоугольный по умолчанию
+    result = 2;
+
+    if (fabs(tmp - side_a * side_a) < FLOAT_ERR)
+        result = 1;
+    else if (tmp > side_a * side_a)
+        result = 0;
+
+    return result;
 }
