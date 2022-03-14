5c0ecb8b483faa0e209651fe930b8e0bed2a7868
sas21u901 <sas21u901@student.bmstu.ru>
2022-03-14 11:25:00 +0300

    task 3 fixed

--- lab_01_03_02/main.c
+++ lab_01_03_02/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
+double count_resistance(double r1, double r2, double r3);
+
 int main(void)
 {
     double r1, r2, r3, r;
     scanf("%lf%lf%lf", &r1, &r2, &r3);
-    r = 1 / (1 / r1 + 1 / r2 + 1 / r3);
+    r = count_resistance(r1, r2, r3);
     printf("%.6lf", r);
 
     return 0;
+}
+
+double count_resistance(double r1, double r2, double r3)
+{
+    return 1 / (1 / r1 + 1 / r2 + 1 / r3);
 }
