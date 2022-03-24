09ca303abd35b30a2a7f17e39ce06f887d71bf71
sas21u901 <sas21u901@bmstu.ru>
2022-03-24 22:43:37 +0300

    task 2 small changes

--- lab_02_02_02/main.c
+++ lab_02_02_02/main.c
@@ -82,7 +82,8 @@ int correct_number(int n)
 
     int i = 0;
     
-    do {
+    do 
+    {
         last_digit = (n % (int)pow(10, i + 1)) / (int)pow(10, i);
         ++i;
     } while (n / (int)pow(10, i) != 0);
