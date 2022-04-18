cef5959b6687c8b02beec6cf06a7cac26e454e2a
sas21u901 <sas21u901@bmstu.ru>
2022-04-18 11:24:56 +0300

    task 4:added swap function

--- lab_02_04_02/main.c
+++ lab_02_04_02/main.c
@@ -16,6 +16,8 @@ void print_array(int *arr, int size);
 
 void bubble_sort(int *arr, int size);
 
+void swap(int *a, int *b);
+
 int main(void)
 {
     int exit_code = OK;
@@ -81,13 +83,16 @@ void print_array(int *arr, int size)
 
 void bubble_sort(int *arr, int size)
 {
-    int tmp = 0;
     for (int i = 0; i < size; ++i)
         for (int j = 0; j < size - i - 1; ++j)
             if (arr[j] > arr[j + 1])
-            {
-                tmp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = tmp;
-            }
+                swap(arr + j, arr + j + 1);
+}
+
+void swap(int *a, int *b)
+{
+    int tmp = 0;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
