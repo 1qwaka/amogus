6d5dbf6f118b37ebc7826b178f9b35dd96b2ecdf
sas21u901 <sas21u901@student.bmstu.ru>
2022-06-10 17:54:48 +0300

    task 3 fix

--- lab_05_03/utils.c
+++ lab_05_03/utils.c
@@ -42,20 +42,29 @@ int print_nums(FILE *f)
 int sort_file(FILE *f)
 {
     int rc = OK;
-    long size = size_of_file(f) / sizeof(num_t);
+    long size_bytes = size_of_file(f);
 
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size - i - 1; j++)
-        {
-            num_t cur = get_number_by_pos(f, j);
-            num_t next = get_number_by_pos(f, j + 1);
-            if (cur > next)
+    if (size_bytes != 0 && size_bytes % sizeof(num_t) == 0)
+    {
+        long size = size_bytes / sizeof(num_t);
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size - i - 1; j++)
             {
-                put_number_by_pos(f, j, next);
-                put_number_by_pos(f, j + 1, cur);
-            }
-        }    
+                num_t cur = get_number_by_pos(f, j);
+                num_t next = get_number_by_pos(f, j + 1);
+                if (cur > next)
+                {
+                    put_number_by_pos(f, j, next);
+                    put_number_by_pos(f, j + 1, cur);
+                }
+            }    
+    }
+    else
+    {
+        rc = ERR_DATA;
+    }
 
+   
     return rc;
 }
 
