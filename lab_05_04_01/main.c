eb6ac2ed0bea504b5b98fd85b2206db4ab3f1588
sas21u901 <sas21u901@student.bmstu.ru>
2022-06-11 13:07:13 +0300

    task 4 fix

--- lab_05_04_01/main.c
+++ lab_05_04_01/main.c
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
     } 
     else if (argc == 3 && strcmp("db", argv[1]) == 0)
     {
-        // return 53;
+        return 53;
 
         FILE *f = fopen(argv[2], "rb+");
         if (f != NULL)
